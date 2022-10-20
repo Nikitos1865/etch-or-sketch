fa6ecc5c54fc2f11e1aa9a80b9f711641d4c4465
Nikita Angarski <na2789@columbia.edu>
2022-10-19 23:15:42 -0400

    Finished the method that allows user to select how many squares per row/column, added a text input box and controlled for the results to give user as many options as possible between 4 and 100. Added an error message that fades in and out to show the user that their input is not valid. Learned how to use timeOut to run functions with a delay, and used this to fade the error message in and out

--- javascript/etch.js
+++ javascript/etch.js
@@ -1,19 +1,52 @@
 const gridContainer = document.createElement('div');
-gridContainer.className = 'gridContainer';
+gridContainer.id = 'grid-container';
 
-const grid = document.createElement('div');
-gridContainer.appendChild(grid);
+let numRows = 100;
 
-function createGrids() {
-    for(let i = 0; i<100; i++) {
+let numGrids = numRows**2;
+
+
+
+function createGrids(num) {
+    for(let i = 0; i<num; i++) {
         let grid = document.createElement('div');
-        grid.className = "gridBox";
+        grid.className = "grid-box";
         gridContainer.appendChild(grid);
     }
+    document.body.appendChild(gridContainer);
 }
 
-createGrids() 
+createGrids(numGrids) 
 
-document.body.appendChild(gridContainer);
+const inputSquares = document.createElement("INPUT");
+inputSquares.setAttribute("type","text");
+document.getElementById("grid-number").appendChild(inputSquares);
 
+const setGridAmount = document.createElement('button');
+setGridAmount.textContent = "Set grid size!"
+document.getElementById("grid-number").appendChild(setGridAmount);
+
+const errorMessage = document.createElement("div")
+errorMessage.textContent = "Error: this is not a valid input"
+document.getElementById("grid-number").appendChild(errorMessage);
+errorMessage.classList.add("error"); 
+
+
+
+function readInRows() {
+    num = Number(inputSquares.value)
+    if (num <= 100 && num >= 4) {
+        document.querySelectorAll(".grid-box").forEach(el => el.remove());
+        document.getElementById('grid-container').style.gridTemplateColumns = "repeat("+num+","+960/num+"px)";
+        document.getElementById('grid-container').style.gridTemplateRows = "repeat("+num+","+960/num+"px)";
+        createGrids(num**2);
+    }
+    else 
+    errorMessage.classList.toggle('fade');
+    setTimeout(() => {
+        errorMessage.classList.remove('fade');
+    }, "1700");
+}
+
+setGridAmount.addEventListener('click', () => {readInRows()})
   
